9628351b52ed00171130dabce35c7c4b83580dcd
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2022-11-13 17:13:21 +0000

    レビュー編集まで

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -5,6 +5,8 @@ class Public::BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @review = Review.new
+    @reviews = Review.all
+    @rreview = current_user.reviews.where(book_id: @book.id)
   end
 
 
