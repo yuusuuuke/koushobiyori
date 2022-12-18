4d8f292266bf2405bcc31007796325c8d7563872
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2022-12-18 05:29:33 +0000

    public/books/show-count.js修正

--- app/javascript/packs/count.js
+++ app/javascript/packs/count.js
@@ -19,6 +19,26 @@ $(document).on('turbolinks:load', function () {
     } else {
       $(".js-comment-count").css("color","black");
     }
-    $(".js-comment-count").text(count + "/200文字");
+    $(this).parents().children(".js-comment-count").text(count + "/200文字");
+  });
+  
+    var review_count = $(".js-edit-comment").text().replace(/\n/g, "改行").length;
+  //文字数がオーバーしていたら文字色を赤にする
+  // if (count > 200) //→編集画面などで、すでに入力がある場合に必要
+  //   $(".js-comment-count").css("color","red");
+  // }
+  $(".js-edit-comment-count").text( review_count + "/200文字");
+
+  $(".js-edit-comment").on("keyup", function() {
+    // 処理（キーボードを押した時、フォームに残り何文字入力できるかを数えて表示する）
+    //フォームのvalueの文字数を数える
+    var review_count = $(this).val().replace(/\n/g, "改行").length;
+    // var now_count = 200 - count;
+    if (review_count > 200) {
+      $(".js-edit-comment-count").css("color","red");
+    } else {
+      $(".js-edit-comment-count").css("color","black");
+    }
+    $(".js-edit-comment-count").text(review_count + "/200文字");
   });
 });
